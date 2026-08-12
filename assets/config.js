87ea8a5300cca7880723c76c5613fedeab4cd524
ACG游戏社 <acgyxs@gmail.com>
2026-08-13 02:02:33 +0800

    Add release page configuration

---
+++ assets/config.js
@@ -0,0 +1,30 @@
+window.YKACG_CONFIG = {
+  releaseDomain: "ykacg.net",
+  mainSite: {
+    title: "进入鸢空ACG",
+    subtitle: "www.ykacg.com · 官方主站",
+    url: "https://www.ykacg.com/"
+  },
+  backups: [
+    { domain: "ykacg.cc", label: "备用线路 01", url: "https://ykacg.cc/" },
+    { domain: "ykacg.top", label: "备用线路 02", url: "https://ykacg.top/" },
+    { domain: "ykacg.vip", label: "备用线路 03", url: "https://ykacg.vip/" }
+  ],
+  links: {
+    app: {
+      title: "APP下载",
+      subtitle: "Android 客户端",
+      url: ""
+    },
+    group: {
+      title: "加入群聊",
+      subtitle: "QQ群 / 最新通知",
+      url: "https://qm.qq.com/q/asuinwSixq"
+    },
+    contact: {
+      title: "联系站长",
+      subtitle: "Telegram / 联系方式",
+      url: ""
+    }
+  }
+};
