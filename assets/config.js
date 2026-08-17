a06c7fa7719e9ed5764afecc177c1972d700a77a
ACG游戏社 <acgyxs@gmail.com>
2026-08-17 09:38:10 +0800

    Update APP download link to 1.0.3

--- assets/config.js
+++ assets/config.js
@@ -14,7 +14,7 @@ window.YKACG_CONFIG = {
     app: {
       title: "APP下载",
       subtitle: "鸢空ACG 1.0.2 · Android",
-      url: "https://app.ykacg.com/%E9%B8%A2%E7%A9%BAACG%20V1.0.2/YKACG_1.0.2.apk"
+      url: "https://app.ykacg.com/%E9%B8%A2%E7%A9%BAACG%20V1.0.3/YKACG_1.0.3.apk"
     },
     group: {
       title: "加入群聊",
